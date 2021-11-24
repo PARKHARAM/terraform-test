66649668b084d08038be9a3466875c6181b02d7f
PARKHARAM <33815176+PARKHARAM@users.noreply.github.com>
2021-11-24 13:42:55 +0900

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,12 @@
+
+terraform{
+    required_providers{
+        google = " ~> 3.5"
+    }
+
+    backend "gcs"{
+       
+        bucket      = "haram-bucket2"
+        prefix      = "jenkins.terraform.tfstate"
+    }
+}

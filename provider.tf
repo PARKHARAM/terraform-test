36d91142f3c8b0428d6e32db01af239efa7e6e43
PARKHARAM <33815176+PARKHARAM@users.noreply.github.com>
2021-11-24 13:03:22 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
   version = "3.5.0"
-  credentials = file("C:/key/haram.json")
+  #credentials = file("C:/key/haram.json")
   project = "haram-326012"
   region  = "asia-northeast3"
   zone    = "asia-northeast3-b"
